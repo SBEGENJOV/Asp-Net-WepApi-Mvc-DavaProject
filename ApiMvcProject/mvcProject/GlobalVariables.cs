cf65ffed7140f9a9b2c47f32780e220b0760b40c
SBEGENJOV <seydi19981995@gmail.com>
2023-10-21 16:36:08 +0300

    21.10.2023

---
+++ ApiMvcProject/mvcProject/GlobalVariables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Handlers;
+using System.Net.Http.Headers;
+
+namespace mvcProject
+{
+    public class GlobalVariables
+    {
+
+        public static HttpClient webapiclient=new HttpClient();
+        static GlobalVariables()
+        {
+            webapiclient.BaseAddress = new Uri("https://localhost:44366/api/");
+            webapiclient.DefaultRequestHeaders.Clear();
+            webapiclient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
